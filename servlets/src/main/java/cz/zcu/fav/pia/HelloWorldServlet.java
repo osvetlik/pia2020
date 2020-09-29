a0cbef280db368eb9451ec7cab9dfc62205ecc68
Ondřej Světlík <ondrej.svetlik@inventi.cz>
2020-09-29 12:22:15 +0200

    Fixing web app configuration + implementing a simple servlet

--- servlets/src/main/java/cz/zcu/fav/pia/HelloWorldServlet.java
+++ servlets/src/main/java/cz/zcu/fav/pia/HelloWorldServlet.java
@@ -1,8 +1,23 @@
 package cz.zcu.fav.pia;
 
-import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
 
-@WebServlet
-public class HelloWorldServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet({
+	"/hello"
+})
+public class HelloWorldServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 2882987200487771166L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getOutputStream().print("Hello world!");
+	}
 
 }
